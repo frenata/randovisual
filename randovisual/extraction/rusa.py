d77e06e502d9ee62dddaae7432bbfbb78935132c
Andrew Nichols <andrew@frenata.net>
2025-11-25 19:58:16 -0500

    parse ride results

--- randovisual/extraction/rusa.py
+++ randovisual/extraction/rusa.py
@@ -1,3 +1,4 @@
+import datetime
 from pydantic import BaseModel
 import functools
 import logging
@@ -27,6 +28,13 @@ class Member(BaseModel):
     def add_year(self, year):
         self.years.add(year)
 
+
+class Ride(BaseModel):
+    id: int
+    link: str
+    date: datetime.date
+    duration: datetime.timedelta
+
 def find_rusa_members(year: int):
     rusa_members = dict()
 
@@ -55,3 +63,53 @@ def find_rusa_members(year: int):
 
     logger.info(f"Found {len(rusa_members)} total RUSA members in {year} ride data.")
     return rusa_members
+
+def parse_ride(tag):
+    """
+    <tr class="individual-ride-result">
+    <td align="left">RUSA-T132670</td>
+    <td align="left">RUSAT</td>
+    <td align="right">101</td>
+    <td align="left">2025/07/13</td>
+    <td align="left"></td>
+    <td align="left"><a href="/cgi-bin/permview_GF.pl?permid=3016">FL: Panhandle Populaire</a></td>
+    <td align="left">04:12</td>
+    <td align="center"></td>
+    </tr>
+    """
+
+    try:
+        date = datetime.date.strptime(tag.select_one("td:nth-child(4)").text, "%Y/%m/%d")
+        time = re.search(r'(\d+):(\d+)', tag.select_one("td:nth-child(7)").text)
+        duration = datetime.timedelta(hours=int(time.group(1)), minutes=int(time.group(2)))
+        route_link = f"https://rusa.org{tag.select_one("td:nth-child(6) > a").attrs["href"]}"
+
+        if "permid" not in route_link:
+            logger.info("not a permanent, skipping")
+            return None
+        permid = int(re.search(r'permid=(\d+)', route_link).group(1))
+
+        ride = Ride(id=permid, link=route_link, date=date, duration=duration)
+        # breakpoint()
+        pass
+        return ride
+    except:
+        logger.exception("error parsing ride result")
+        return None
+
+
+
+
+
+def find_rider_results(member: Member):
+    logger.info(f"Requesting ride data for {member.id} ...")
+
+    keys = ["mid", "sname", "club", "sortby", "regid", "type", "dist", "date", "rtid", "esortby", "permid", "permdate", "award", "year", "submit"]
+    payload = {**{key: "" for key in keys}, "mid": str(member.id), "sortby": "date", "esortby": "name", "submit": "search"}
+    # mid=14039&sname=&club=&sortby=date&regid=&type=&dist=&date=2024&rtid=&esortby=name&permid=&permdate=&award=&year=&submit=search
+    response = requests.post("https://rusa.org/cgi-bin/resultsearch_PF.pl", data=payload)
+    response.raise_for_status()
+
+    html = BeautifulSoup(response.text, features="html.parser")
+    results = html.select("tr.individual-ride-result")
+    rides = list(filter(bool, map(parse_ride, results)))
